df2949101ad20c2e590c1b6231e32acd40b9731e
Hornet <50376785+g-lesssard@users.noreply.github.com>
2021-09-19 11:48:54 -0400

    SWARINFO-499 Hiveboard bringup (#30)

--- src/bsp/src/esp/hal/pin_map.h
+++ src/bsp/src/esp/hal/pin_map.h
@@ -14,24 +14,32 @@
  */
 
 // Change to SPI_CONFIG_SOC to test communication with SOC
-#define SPI_CONFIG_WROOM
+#define SPI_CONFIG_HIVEBOARD
 
 // SPI Link to STM
 #define STM_SPI VSPI_HOST
 #define STM_MOSI_D GPIO_NUM_23
 #define STM_MISO_Q GPIO_NUM_19
 #define STM_CLK GPIO_NUM_18
-// Different CS for WROOM and SOC
+// Different CS for WROOM, SOC and HiveBoard
+#ifdef SPI_CONFIG_HIVEBOARD
+#define STM_CS GPIO_NUM_5
+#endif
 #ifdef SPI_CONFIG_WROOM
 #define STM_CS GPIO_NUM_34
-#elif SPI_CONFIG_SOC
+#endif
+#ifdef SPI_CONFIG_SOC
 #define STM_CS GPIO_NUM_17
 #endif
 
 // DMA_CHANNELS
-#define DMA_CHANNEL_SPI 1 // No dma
+#define DMA_CHANNEL_SPI 1
 
 // User GPIOs
+#ifdef SPI_CONFIG_HIVEBOARD
+#define STM_USER_0 GPIO_NUM_25
+#define STM_USER_0_PIN_MASK GPIO_SEL_25
+#else
 #define STM_USER_0 GPIO_NUM_5
 #define STM_USER_0_PIN_MASK GPIO_SEL_5
 // Different user pins for WROOM and SOC
@@ -41,6 +49,7 @@
 #elif SPI_CONFIG_SOC
 #define STM_USER_1 GPIO_NUM_27
 #define STM_USER_1_PIN_MASK GPIO_SEL_27
+#endif
 #endif
 
 // Output LED
