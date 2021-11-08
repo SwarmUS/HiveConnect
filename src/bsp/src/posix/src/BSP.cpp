79fd50c365b9ca1e652e130a19fe899f762ca2c6
Xavier Groleau <xavgroleau@gmail.com>
2021-11-08 08:32:30 -0500

    Fix CPU usage (#37)
    * partial fixes?
    
    * Format files

--- src/bsp/src/posix/src/BSP.cpp
+++ src/bsp/src/posix/src/BSP.cpp
@@ -1,16 +1,31 @@
 #include "BSP.h"
-#include "BaseTask.h"
+#include <Task.h>
 #include <thread>
 
 constexpr uint8_t gc_spinnerCores = 1;
 
+/**
+ * @brief Task that kills OS when ROS node is stopped
+ */
+void rosWatcher(void* param) {
+    (void)param;
+    const int loopRate = 100;
+
+    while (ros::ok()) {
+        ros::spinOnce();
+        Task::delay(loopRate);
+    }
+}
+
 BSP::~BSP() = default;
 
-BSP::BSP(const ros::NodeHandle& nodeHandle) : m_spinner(gc_spinnerCores) {
+BSP::BSP(const ros::NodeHandle& nodeHandle) :
+    m_rosWatchTask("ros_watch", tskIDLE_PRIORITY + 1, rosWatcher, NULL),
+    m_spinner(gc_spinnerCores) {
     m_rosNodeHandle = std::make_shared<ros::NodeHandle>(nodeHandle);
 }
 
-void BSP::initChip() { m_spinner.start(); }
+void BSP::initChip() { m_rosWatchTask.start(); }
 
 ChipInfo BSP::getChipInfo() const {
     return ChipInfo{.m_cores = (uint8_t)std::thread::hardware_concurrency(),
